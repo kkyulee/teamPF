258c7f1b27b8f3658a26813c8dc884c655811bb4
이준현 <115512931+LeeJoNyun@users.noreply.github.com>
2025-06-09 15:36:22 +0900

    bb

--- js/main.js
+++ js/main.js
@@ -264,6 +264,17 @@ const fnScrollEvent = () => {
 
 
 }
+
+const fnSetHeaderMenu = () => {
+    const BASE_URL = window.location.origin + "/teamPF";
+    const menu = document.querySelector(".menu");
+    let html = `<li><a href="${BASE_URL}/kor/category/index.html">업무분야</a></li>
+        <li><a href="${BASE_URL}/kor/member/index.html">구성원</a></li>
+        <li><a href="${BASE_URL}/kor/mediacenter/index.html">미디어센터</a></li>
+        <li><a href="${BASE_URL}/kor/people/index.html">인재영입</a></li>
+        <li><a href="${BASE_URL}/kor/about/index.html">법인소개</a></li>`;
+    menu.innerHTML = html;
+}
 (() => {
 
     fnCreateBanner();
@@ -276,4 +287,6 @@ const fnScrollEvent = () => {
     fnCreateExample();
 
     fnScrollEvent();
+
+    fnSetHeaderMenu();
 })();
